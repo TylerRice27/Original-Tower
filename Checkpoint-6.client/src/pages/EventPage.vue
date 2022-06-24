ddb452fa892974cb52863060cc31f8443e2155e3
TylerRice27 <Tylerrayrice2@gmail.com>
2022-06-23 20:58:00 -0600

    Halfway through even details page

--- Checkpoint-6.client/src/pages/EventPage.vue
+++ Checkpoint-6.client/src/pages/EventPage.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="event-page">
-    <section id="towerEvent">
-      <img :src="towerEvent.coverImg" alt="" />
-      <div>
+  <div class="event-page card bg-dark m-3">
+    <section class="d-flex m-3" id="towerEvent">
+      <img class="tower-pic img-fluid" :src="towerEvent.coverImg" alt="" />
+      <div class="m-3">
         <h4>{{ towerEvent.name }}</h4>
+        <h5 class="text-success">{{ towerEvent.location }}</h5>
+        <p class="mt-5">{{ towerEvent.description }}</p>
+        <h3 class="mt-5 pt-3">{{ towerEvent.capacity }} Spots Left</h3>
+        <button class="btn btn-primary mt-5">
+          Attend <i class="mdi mdi-thumb-up"></i>
+        </button>
       </div>
+      <h6 class="text-end" v-if="towerEvent.startDate">
+        {{ formatDate(towerEvent.startDate) }}
+      </h6>
     </section>
+  </div>
+  <p class="m-3">Who is Attending</p>
+  <div class="row">
+    <div class="col-md-12 card bg-dark"></div>
   </div>
 </template>
 
@@ -32,8 +45,27 @@ export default {
     })
 
     return {
-      towerEvent: computed(() => AppState.activeEvent)
+      towerEvent: computed(() => AppState.activeEvent),
+
+
+
+
+
+      formatDate(rawDate) {
+        let time = new Date(rawDate)
+        // let month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(time)
+        let day = time.toDateString()
+        // let year = time.getUTCFullYear()
+        return ` ${day}`
+      }
     }
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.tower-pic {
+  width: 500px;
+}
+</style>
