a5153fda9e1e0631e194634d5e19b644bbc3c151
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-12 10:07:46 -0600

    Deploy Push

--- Checkpoint-6.client/vite.config.js
+++ Checkpoint-6.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../Checkpoint-6/Checkpoint-6/client',
     sourcemap: false
   },
   server: {
