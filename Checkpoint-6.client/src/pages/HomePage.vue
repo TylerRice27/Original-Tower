fbdbc8e82ac2169dd4c5940ee73385a9edcdfa2d
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-15 22:25:09 -0600

    img change

--- Checkpoint-6.client/src/pages/HomePage.vue
+++ Checkpoint-6.client/src/pages/HomePage.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="home container">
-    <div class="mt-2 justify-content-center img-fluid">
-      <img
-        class="cool img-fluid"
-        src="https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-        alt=""
-      />
+    <div class="mt-2 row justify-content-center">
+      <div class="cool" alt="">
+        <h2 class="txt">Tower</h2>
+      </div>
     </div>
     <div class="row card bg-dark px-3 rounded mt-4">
       <div class="d-flex justify-content-between col-md-12">
@@ -61,6 +59,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.txt {
+}
 .cool {
   height: 24vh;
   width: 100vw;
@@ -69,5 +69,6 @@ export default {
   border: solid;
   border-color: #3500d3;
   border-width: 2px;
+  background-image: url(https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80);
 }
 </style>
