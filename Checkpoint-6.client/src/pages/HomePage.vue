bddd4611afde968c9eaf5a682865568cd578ed3d
TylerRice27 <Tylerrayrice2@gmail.com>
2022-08-05 10:02:42 -0600

    Styling

--- Checkpoint-6.client/src/pages/HomePage.vue
+++ Checkpoint-6.client/src/pages/HomePage.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home container">
     <div class="mt-2 row justify-content-center">
-      <div class="cool" alt="">
-        <h2 class="txt">Tower</h2>
-      </div>
+      <div class="cool mt-2" alt=""></div>
     </div>
     <div class="row card bg-dark px-3 rounded mt-4">
       <div class="d-flex justify-content-between col-md-12">
@@ -63,10 +61,11 @@ export default {
   height: 24vh;
   width: 100vw;
   object-fit: cover;
-  filter: blur(2px);
+  background-position: 1110px -342px;
+  // filter: blur(2px);
   border: solid;
   border-color: #3500d3;
   border-width: 2px;
-  background-image: url(https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80);
+  background-image: url(https://images.unsplash.com/photo-1546707012-c46675f12716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Nnx8fGVufDB8fHx8&w=1000&q=80);
 }
 </style>
