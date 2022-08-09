3a70b18a87d0c1505fe0f352cafb6998dd6ab1ff
TylerRice27 <Tylerrayrice2@gmail.com>
2022-08-09 16:43:04 -0600

    Build Push Again

--- Checkpoint-6.client/vite.config.js
+++ Checkpoint-6.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../Checkpoint-6/Checkpoint-6/client',
+    outDir: '../Checkpoint-6/client',
     sourcemap: false
   },
   server: {
--- Checkpoint-6/client/app/env.js
+++
@@ -1,7 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'dev-ukxg-q38.us.auth0.com'
-export const audience = 'https://TylerDev.com'
-export const clientId = 'OrNg1n6swOJ7RGMg8DMDMQMPRnqipYnu'
-
-
